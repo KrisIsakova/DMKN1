2e9e657c16240302943dd2f4bfb70ac3588b19c7
KrisIsakova <118714648+KrisIsakova@users.noreply.github.com>
2022-11-21 09:50:46 +0300

    Add files via upload

---
+++ домашняя контрольная.pas
@@ -0,0 +1,15 @@
+﻿program znachfunv;
+var x,y:real;
+begin
+  x:=-11;
+  while x<=5 do
+  begin
+    if x<-9 then y:=ln(x)/tan(x)-power(x,(0.1*x))/power(x,(1/3))
+else  if (-9<=x) and (x<-3) then y:=5-power(x,3)
+else  if (-3<=x) and (x<3) then y:=cos(2*x)/power(x,1/3)+cos(x)
+else if 3<=x then y:=power(e,x/power(x,0.1*x)-68/power(x,1/3));
+writeln('x',x:2:2,' ; ','y ',y:2:2);
+x:=x+0.3
+  end;
+end.
+
